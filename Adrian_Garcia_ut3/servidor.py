ff7be525e778bff9f8b2237dfba35a1a6e368bc2
Vincenzo-Rojas <vincenzo.rojas.carrera@gmail.com>
2025-12-18 16:03:45 +0100

    servidor

---
+++ Adrian_Garcia_ut3/servidor.py
@@ -0,0 +1,12 @@
+import socket
+import json
+from threading import Thread, Lock
+
+# Configuración del servidor
+direccion = ("127.0.0.1", 5000)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sock.bind(direccion)
+sock.listen(5)
